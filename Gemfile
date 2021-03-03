0c2d0945f284f308d3b3ae3f84cd1f4647b37076
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2021-03-03 11:45:37 +0300

    cable adapter redis

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'delayed_job_active_record'
 gem 'unicorn'
+gem "redis", "~> 3.0"
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- app/controllers/visitor_controller.rb
+++ app/controllers/visitor_controller.rb
@@ -3,10 +3,7 @@ class VisitorController < ApplicationController
   end
 
   def hard_method
-    ActionCable.server.broadcast 'finish_process', {process_name: "FIRST"}
-
     process_name = 'HARD METHOD'
-    HardJob.perform_now(process_name)
-    # HardJob.perform_later(process_name)
+    HardJob.perform_later(process_name)
   end
 end
